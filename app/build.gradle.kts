c1800ee2ded067b90a8a312d111acfebdefc27f4
VSV <mr.vsv92@mail.ru>
2025-09-15 08:46:13 +0700

    gradle structure

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("chirp.spring-boot-app")
+}
+
+group = "com.vsv"
+version = "0.0.1-SNAPSHOT"
+description = "Chirp Backend"
+
+dependencies {
+    implementation(projects.user)
+    implementation(projects.chat)
+    implementation(projects.notification)
+    implementation(projects.common)
+}
---
+++ chat/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java-library")
+    id("chirp.spring-boot-service")
+    kotlin("plugin.jpa")
+}
+
+group = "com.vsv"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation(projects.common)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
