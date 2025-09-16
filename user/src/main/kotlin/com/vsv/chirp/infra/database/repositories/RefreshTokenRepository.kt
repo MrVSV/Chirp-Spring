e7aeff2517e3e90bce9067bb0d38c6144d1e48ee
VSV <mr.vsv92@mail.ru>
2025-09-16 11:10:53 +0700

    refresh token storage

---
+++ user/src/main/kotlin/com/vsv/chirp/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,12 @@
+package com.vsv.chirp.infra.database.repositories
+
+import com.vsv.chirp.domain.model.UserId
+import com.vsv.chirp.infra.database.entities.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository: JpaRepository<RefreshTokenEntity, Long> {
+
+    fun findByUserIdAndHashedToken(userId: UserId, hashedToken: String): RefreshTokenEntity?
+    fun deleteByUserIdAndHashedToken(userId: UserId, hashedToken: String)
+    fun deleteByUserId(userId: UserId)
+}
