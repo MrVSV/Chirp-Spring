3c61ece1cd1739035c77151b13bae15f14a1a672
VSV <mr.vsv92@mail.ru>
2025-09-20 21:31:56 +0700

    test send email

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(projects.common)
 
     implementation(libs.kotlin.reflect)
+    implementation(libs.spring.boot.starter.mail)
     implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.data.redis)
--- notification/src/main/kotlin/com/vsv/chirp/infra/message_queue/NotificationUserEventListener.kt
+++ notification/src/main/kotlin/com/vsv/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -1,12 +1,15 @@
 package com.vsv.chirp.infra.message_queue
 
 import com.vsv.chirp.domain.events.user.UserEvent
+import com.vsv.chirp.service.EmailService
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 @Component
-class NotificationUserEventListener {
+class NotificationUserEventListener(private val emailService: EmailService) {
 
     @RabbitListener(queues = [MessageQueues.NOTIFICATION_USER_EVENTS])
     @Transactional
@@ -14,12 +17,31 @@ class NotificationUserEventListener {
         when (event) {
             is UserEvent.Created -> {
                 println("User created")
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
             }
             is UserEvent.RequestResendVerification -> {
                 println("Request resend verification")
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
             }
             is UserEvent.RequestResetPassword -> {
                 println("Request reset password")
+                emailService.sendPasswordResetEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.resetPasswordToken,
+                    expiresIn = Duration.ofMinutes(event.expiresInMinutes)
+                )
             }
             else -> Unit
         }
